ac0b278c219ab884ff385c4b9b7240aa1f007611
shoshsf <72558473+shoshsf@users.noreply.github.com>
2022-11-04 04:11:55 -0400

    Changes to loading spinner microinteraction

--- Beta Build/script.js
+++ Beta Build/script.js
@@ -55,38 +55,123 @@ button.addEventListener('transitionend', addClass);
       /*-------------------*/
 
 
-function onBtnClickHandle1(){
-  setTimeout( function() { 
-  window.open ("https://drexel.edu/equity-inclusive-culture/diversity/anti-racism-task-force/", '_blank');
-}, 1000); /* nEED TO ADJUST TIME BASED ON onclick animation */
-}
+// function onBtnClickHandle1(){
+//   setTimeout( function() { 
+//   window.open ("https://drexel.edu/equity-inclusive-culture/diversity/anti-racism-task-force/", '_blank');
+// }, 2000); /* nEED TO ADJUST TIME BASED ON onclick animation */
+// }
 
-function onBtnClickHandle2(){
-  setTimeout( function() { 
-  window.open ("https://drexel.edu/disability-resources/support-accommodations/employee-resources/", '_blank');
-}, 1000); /* nEED TO ADJUST TIME BASED ON onclick animation */
-}
+// function onBtnClickHandle2(){
+//   setTimeout( function() { 
+//   window.open ("https://drexel.edu/disability-resources/support-accommodations/employee-resources/", '_blank');
+// }, 2000); /* nEED TO ADJUST TIME BASED ON onclick animation */
+// }
 
-function onBtnClickHandle3(){
-  setTimeout( function() { 
-  window.open ("https://www.census.gov/newsroom/stories/diversity-awareness.html", '_blank');
-}, 1000); /* nEED TO ADJUST TIME BASED ON onclick animation */
-}
+// function onBtnClickHandle3(){
+//   setTimeout( function() { 
+//   window.open ("https://www.census.gov/newsroom/stories/diversity-awareness.html", '_blank');
+// }, 2000); /* nEED TO ADJUST TIME BASED ON onclick animation */
+// }
 
-function onBtnClickHandle4(){
-  setTimeout( function() { 
-  window.open ("https://drexel.edu/equity-inclusive-culture/training/media/", '_blank');
-}, 1000); /* nEED TO ADJUST TIME BASED ON onclick animation */
-}
-
-
-
-/* For button Load animation */
-
-const btn = document.querySelector(".button");
-
-btn.classList.add("button--loading");
-btn.classList.remove("button--loading");
-/*--------------------------------*/
+// function onBtnClickHandle4(){
+//   setTimeout( function() { 
+//   window.open ("https://drexel.edu/equity-inclusive-culture/training/media/", '_blank');
+// }, 2000); /* nEED TO ADJUST TIME BASED ON onclick animation */
+// }
 
 
+
+// /* For button Load animation */
+
+// const btn = document.querySelector(".button");
+
+// btn.classList.add("button--loading");
+// btn.classList.remove("button--loading");
+// /*--------------------------------*/
+
+// ---------------Loading Spinner----------------------
+      // selecting the button and user feedback box
+      const button1 = document.querySelector('#js-start-anim1')
+      const userFeedback1 = document.querySelector('.loading1')
+
+      // the CSS animation end event listener
+      userFeedback1.addEventListener('animationend', () => {
+        // userFeedback.textContent = ' Learn More'
+        userFeedback1.classList.remove('loading--anim1')
+        // userFeedback.classList.add('user-feedback--anim-done')
+        window.open ("https://drexel.edu/equity-inclusive-culture/diversity/anti-racism-task-force/", '_blank', 2000)
+      })
+
+      // the button click adds / starts the CSS animation
+      button1.addEventListener('click', () => {
+        // userFeedback.classList.add('button--loading');
+        userFeedback1.classList.add('loading--anim1');
+      })
+
+
+      // -------------------------------------------
+
+
+      // selecting the button and user feedback box
+      const button2 = document.querySelector('#js-start-anim2')
+      const userFeedback2 = document.querySelector('.loading2')
+
+      // the CSS animation end event listener
+      userFeedback2.addEventListener('animationend', () => {
+        // userFeedback.textContent = ' Learn More'
+        userFeedback2.classList.remove('loading--anim2');
+        // userFeedback.classList.add('user-feedback--anim-done')
+        window.open ("https://drexel.edu/disability-resources/support-accommodations/employee-resources/", '_blank', 2000);
+      })
+
+      // the button click adds / starts the CSS animation
+      button2.addEventListener('click', () => {
+        // userFeedback.classList.add('button--loading');
+        userFeedback2.classList.add('loading--anim2');
+      })
+
+
+      // -------------------------------------------
+
+
+      // selecting the button and user feedback box
+      const button3 = document.querySelector('#js-start-anim3');
+      const userFeedback3 = document.querySelector('.loading3');
+
+      // the CSS animation end event listener
+      userFeedback3.addEventListener('animationend', () => {
+        // userFeedback.textContent = ' Learn More'
+        userFeedback3.classList.remove('loading--anim3');
+        // userFeedback.classList.add('user-feedback--anim-done')
+        window.open ("https://www.census.gov/newsroom/stories/diversity-awareness.html", '_blank', 2000);
+      });
+
+      // the button click adds / starts the CSS animation
+      button3.addEventListener('click', () => {
+        // userFeedback.classList.add('button--loading');
+        userFeedback3.classList.add('loading--anim3');
+      });
+
+
+      // -------------------------------------------
+
+
+      // selecting the button and user feedback box
+      const button4 = document.querySelector('#js-start-anim4');
+      const userFeedback4 = document.querySelector('.loading4');
+
+      // the CSS animation end event listener
+      userFeedback4.addEventListener('animationend', () => {
+        // userFeedback.textContent = ' Learn More'
+        userFeedback4.classList.remove('loading--anim4');
+        // userFeedback.classList.add('user-feedback--anim-done')
+      });
+
+      // the button click adds / starts the CSS animation
+      button4.addEventListener('click', () => {
+        // userFeedback.classList.add('button--loading');
+        userFeedback4.classList.add('loading--anim4');
+        window.open ("https://drexel.edu/equity-inclusive-culture/training/media/", '_blank', 2000);
+      });
+
+//---------------Loading Spinner End----------------------
